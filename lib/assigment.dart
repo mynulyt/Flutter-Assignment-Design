d37ac89a6b5071cf411ab24c6324b5a23e075f9c
Mynul Alam <mynulalam27@gmail.com>
2025-07-28 22:26:03 +0600

    assignment

---
+++ lib/assigment.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AssigmentPage extends StatelessWidget {
+  const AssigmentPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
