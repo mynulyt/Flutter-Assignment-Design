72b0dcd1e3bf18653836038d08530711a0a97ffe
Mynul Alam <mynulalam27@gmail.com>
2025-07-28 22:28:11 +0600

    assignment

--- lib/assigment.dart
+++ lib/assigment.dart
@@ -1,10 +1,63 @@
 import 'package:flutter/material.dart';
 
-class AssigmentPage extends StatelessWidget {
-  const AssigmentPage({super.key});
+class AssignmentModule10 extends StatelessWidget {
+  const AssignmentModule10({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            SizedBox(height: 60),
+            Text(
+              "Hello, World!",
+              style: TextStyle(
+                color: Colors.red,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              "Welcome to Flutter!",
+              style: TextStyle(
+                color: Colors.black87,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Image.network(
+              "https://tse1.mm.bing.net/th/id/OIP.X0KMz7V4ozhJiB1l-4V1ZwHaEK?w=1280&h=720&rs=1&pid=ImgDetMain&o=7&rm=3",
+            ),
+            SizedBox(
+              height: 60,
+              width: 400,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+                onPressed: () {
+                  ScaffoldMessenger.of(
+                    context,
+                  ).showSnackBar(SnackBar(content: Text("Button Presses")));
+                },
+                child: Text(
+                  "Press Me",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
